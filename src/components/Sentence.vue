4a62506c05229fcd72df33f0f4d0c128ef1c5499
Alex <revolalex@gmail.com>
2020-08-26 13:50:15 +0200

    step 7 finish

--- src/components/Sentence.vue
+++ src/components/Sentence.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    <p>It's time to start working you have x tasks to do !!</p>
-    <p>Continue that way and you will finish soon, still have x/y tasks</p>
-    <p>Good !! you made half or more x/y tasks</p>
-    <p>Congratulation !! You finished all the x tasks</p>
+    <p
+      v-if="numberOfToDo() == numberOfToDoTrue()"
+      id="timeToWork"
+    ><b-icon-emoji-angry class="iconRight"></b-icon-emoji-angry>It's time to start working you have {{numberOfToDoTrue()}} tasks to do !!<b-icon-emoji-angry class="iconLeft"></b-icon-emoji-angry></p>
+    <p
+      v-else-if="numberOfToDoFalse() < halfNumberOfToDo()"
+      id="continue"
+    ><b-icon-emoji-smile class="iconRight"></b-icon-emoji-smile>Continue that way and you will finish soon, still have {{numberOfToDoTrue()}}/{{numberOfToDo()}} tasks<b-icon-emoji-smile class="iconLeft"></b-icon-emoji-smile></p>
+    <p
+      v-else-if="numberOfToDoFalse() >= halfNumberOfToDo() && numberOfToDoFalse() < numberOfToDo()"
+      id="good"
+    >
+      <b-icon-emoji-laughing class="iconRight"></b-icon-emoji-laughing>
+      Good !! you made half or more {{numberOfToDoTrue()}}/{{numberOfToDo()}} tasks
+      <b-icon-emoji-laughing class="iconLeft"></b-icon-emoji-laughing>
+    </p>
+    <p v-if="numberOfToDo() === numberOfToDoFalse()" id="congratulation">
+      <b-icon-emoji-sunglasses class="iconRight"></b-icon-emoji-sunglasses>
+      Congratulation !! You finished all the {{numberOfToDo()}} tasks
+      <b-icon-emoji-sunglasses class="iconLeft"></b-icon-emoji-sunglasses>
+    </p>
   </div>
 </template>
 <script>
@@ -14,5 +31,51 @@ export default {
       type: Array,
     },
   },
+  methods: {
+    numberOfToDo: function () {
+      return this.list.length;
+    },
+    numberOfToDoTrue: function () {
+      let toDoTrue = 0;
+      this.list.forEach((element) => {
+        if (element.todo) {
+          toDoTrue++;
+        }
+      });
+      return toDoTrue;
+    },
+    halfNumberOfToDo: function () {
+      return this.list.length / 2;
+    },
+    numberOfToDoFalse: function () {
+      let toDoFalse = 0;
+      this.list.forEach((element) => {
+        if (element.todo == false) {
+          toDoFalse++;
+        }
+      });
+      return toDoFalse;
+    },
+  },
 };
 </script>
+<style>
+#timeToWork {
+  color: red;
+}
+#continue {
+  color: orange;
+}
+#good {
+  color: rgb(41, 226, 41);
+}
+#congratulation {
+  color: rgb(5, 160, 26);
+}
+.iconRight {
+  padding-right: 2em;
+}
+.iconLeft {
+  padding-left: 1em;
+}
+</style>
